feee8a8a156dd258ca85240e6ff61bf7958677e7
Jakub Lukáš <jakublukas.eclairdesign@gmail.com>
2025-07-25 17:08:02 +0200

    Refactor ball movement logic and add collision detection with walls and paddles

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from turtle import Screen
 from paddle import Paddle
 from ball import Ball
 import time
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT, BACKGROUND_COLOR
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT, BACKGROUND_COLOR, BALL_SIZE
 
 # class: Playground
 # class: Paddle
@@ -20,10 +20,10 @@ r_paddle = Paddle((350, 0))
 ball = Ball()
 
 screen.listen()
-screen.onkey(key="w", fun=l_paddle.moveup)
-screen.onkey(key="s", fun=l_paddle.movedown)
-screen.onkey(key="Up", fun=r_paddle.moveup)
-screen.onkey(key="Down", fun=r_paddle.movedown)
+screen.onkeypress(key="w", fun=l_paddle.moveup)
+screen.onkeypress(key="s", fun=l_paddle.movedown)
+screen.onkeypress(key="Up", fun=r_paddle.moveup)
+screen.onkeypress(key="Down", fun=r_paddle.movedown)
 
 game_is_on = True
 
@@ -31,6 +31,14 @@ while game_is_on:
     time.sleep(0.1)
     ball.move()
     screen.update()
+
+    # detect collision with a wall
+    if ball.ycor() > (SCREEN_HEIGHT // 2) - BALL_SIZE or ball.ycor() < (-SCREEN_HEIGHT // 2) + BALL_SIZE:
+        ball.bounce_y()
+
+    # detect collision with a right paddle
+    if ball.xcor() > 320 and ball.distance(r_paddle) < 100 or ball.xcor() < -320 and ball.distance(l_paddle) < 100:
+        ball.bounce_x()
 
 
 screen.exitonclick()
